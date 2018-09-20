b6457976d6d45beec8f6f74a7104236463e6772a
somepanda <somesweetrain@gmail.com>
2018-09-20 13:34:17 +0300

    proovin saada seda html lehte näitama

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-        res.sendFile(path.resolve('public/views/index.html'))
+    res.sendFile(path.resolve('public/views/index.html'))
 });
 
 module.exports = router;
