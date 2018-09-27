23ad3d9409b0476f58c44f5d8e2722c953380691
somepanda <somesweetrain@gmail.com>
2018-09-28 00:20:36 +0300

    added reading from database

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,83 @@ router.use(bodyParser.urlencoded({extend: true}));
 
 const newsSchema = new mongoose.Schema({
     title: String,
-    news: String
+    news: String,
+    authorId: {type: Number, default: 1}
 });
 
+const usersSchema = new mongoose.Schema({
+    userId: Number,
+    userName: String
+});
+
+const Users = mongoose.model("Users", usersSchema);
+
+/*//Created a user
+Users({userId: 1, userName: "Bridge fan"}).save();
+*/
+
 const News = mongoose.model("News", newsSchema);
+
+router.get('/news', function(req, res, next) {
+
+    News.find({}, (err, news) => {
+
+        news.forEach((news1) => {
+
+            News.findById(news._id).populate(authorId).
+
+        })
+
+    });
+
+    /*News.find({}, (err, news) => {
+        let newsObjects = [];
+
+        news.forEach((oneNews) => {
+
+            let userName = "";
+            let user1;
+
+            console.log("News author id is: " + oneNews.authorId);
+
+            Users.find({}, (err, users) => {
+
+                users.forEach((user) => {
+
+                    console.log("UserId is: " + user.userId);
+
+                    if (user.userId === oneNews.authorId) {
+                        user1 = user.userName;
+                    }
+
+                });
+
+            });
+
+            newsObjects.push(user1);
+        });
+
+        res.send(newsObjects);
+        */
+        /*
+        Users.find({userId: news.body.authorId}, (err, author) => {
+            res.send(author.body.userName)
+        });
+        res.send(news.body.title);
+        res.send(news.body.news);
+        */
+    });
+});
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile(path.resolve('public/views/index.html'))
 });
 
-router.get('/lisa', function(req, res, next) {
+router.get('/add', function(req, res, next) {
     res.sendFile(path.resolve('public/views/lisa.html'))
 });
 
