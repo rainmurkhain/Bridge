7d098ca1d4b251d246b7ace8ad00b9683be81249
Sombzorro <ristallan@gmail.com>
2018-10-23 16:39:48 +0300

    Accept PNGs, removed addNews.js

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,30 @@ mongoose.Promise = global.Promise;
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 
+
+var multer  = require('multer');
+
+
+
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: '/Images/newsPicture',
+        filename: function (req, file, callback) {
+            //TODO: tee filename skeem
+            console.log("Olen siin: " + req.toString());
+            callback( null, file.originalname+ '-' + Date.now());
+        }
+    })
+});
+
+var app = express()
+
 mongoose.connect("mongodb+srv://" + db_user + ":" + db_pass + "@cluster0-djbk6.mongodb.net/Bridge?retryWrites=true", { useNewUrlParser: true });
 
 const newsSchema = new mongoose.Schema({
     title: String,
     news: String,
+    pictureID: String,
     authorId: {type: Number, default: 1}
 });
 
@@ -41,8 +60,6 @@ router.get('/news', function(req, res, next) {
 });
 
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile(path.resolve('public/views/index.html'))
@@ -52,9 +69,18 @@ router.get('/lisa', function(req, res, next) {
     res.sendFile(path.resolve('public/views/lisa.html'))
 });
 
-router.post("/addNews", (req, res) => {
+router.post("/addNews", upload.single("picture"),  (req, res) => {
+    //Teen eraldi fieldid ja siis panen kokku
+    let title = req.body.title;
+    let body = req.body.news;
+    let pictureID = req.file.filename;
+    console.log("pildi ID: " + req.file.filename);
 
-    const myData = new News(req.body);
+    let myData = new News ({
+        title: title,
+        news: body,
+        pictureID: pictureID
+    });
     myData.save()
         .then(item => {
             res.sendFile(path.resolve('public/views/index.html'));
@@ -62,7 +88,22 @@ router.post("/addNews", (req, res) => {
         .catch(err => {
             res.status(400).send("Unable to save to database");
         });
+
+    if (!req.file) {
+        console.log("No file received");
+        return res.send({
+            success: false
+        });
+
+    } else {
+        console.log('file received');
+        return res.send({
+            success: true
+        })
+    }
 });
+
+
 
 router.post("/lisa", (req, res) => {
 
