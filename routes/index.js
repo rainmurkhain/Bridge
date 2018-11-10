0f057f22143e4a49901b15b6e8fe2b51bda7960e
somepanda <somesweetrain@gmail.com>
2018-11-10 22:09:44 +0200

    add local login

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,60 @@ const router = express.Router();
 const path = require('path');
 const mongoose = require("mongoose");
 const nodemailer = require('nodemailer');
-//const password = require('password');
 const bodyParser = require("body-parser");
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}));
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const User = require('../lib/User');
+
 mongoose.Promise = global.Promise;
+
+passport.use(new Strategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (user.password != password) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+    User.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+router.use(bodyParser.json());
+router.use(require('morgan')('combined'));
+router.use(require('cookie-parser')());
+router.use(require('body-parser').urlencoded({ extended: true }));
+router.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+});
+
+router.get('/logout',
+    function(req, res){
+        req.logout();
+        res.redirect('/');
+});
+
 
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
@@ -28,8 +77,6 @@ var upload = multer({
     })
 });
 
-var app = express()
-
 mongoose.connect("mongodb+srv://" + db_user + ":" + db_pass + "@cluster0-djbk6.mongodb.net/Bridge?retryWrites=true", { useNewUrlParser: true });
 
 const newsSchema = new mongoose.Schema({
@@ -38,17 +85,6 @@ const newsSchema = new mongoose.Schema({
     pictureID: String,
     authorId: {type: Number, default: 1}
 });
-
-const usersSchema = new mongoose.Schema({
-    userId: Number,
-    userName: String
-});
-
-const Users = mongoose.model("Users", usersSchema);
-
-/*//Created a user
-Users({userId: 1, userName: "Bridge fan"}).save();
-*/
 
 const News = mongoose.model("News", newsSchema);
 
@@ -67,12 +103,20 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.resolve('public/views/index.html'))
 });
 
-router.get('/lisa', function(req, res, next) {
+router.get('/lisa', require('connect-ensure-login').ensureLoggedIn(), function(req, res, next) {
     res.sendFile(path.resolve('public/views/lisa.html'))
 });
 
 router.get('/meist', function(req, res, next) {
     res.sendFile(path.resolve('public/views/meist.html'))
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve('public/views/login.html'))
+});
+
+router.get('/register', function(req, res, next) {
+    res.sendFile(path.resolve('public/views/register.html'))
 });
 
 router.post("/addNews", upload.single("picture"),  (req, res) => {
@@ -113,7 +157,7 @@ router.post("/addNews", upload.single("picture"),  (req, res) => {
 
 router.post("/meist", (req, res) => {
 
-    var transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.USER,
@@ -121,7 +165,7 @@ router.post("/meist", (req, res) => {
         }
     });
 
-    var mailOptions = {
+    const mailOptions = {
         from: 'bridge.eesti@gmail.com',
         to: req.body.email,
         subject: 'Külastage uut bridzilehte!',
@@ -140,49 +184,25 @@ router.post("/meist", (req, res) => {
     res.redirect("/meist");
 });
 
-module.exports = router;
+router.post('/register', function(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
 
-function callMessagesApi() {
-    var accessToken = oktaSignIn.tokenManager.get("accessToken");
+    const newuser = new User();
+    newuser.username = username;
+    newuser.password = password;
+    newuser.firstname = firstname;
+    newuser.lastname = lastname;
 
-    if (!accessToken) {
-        return;
-    }
-
-    // Make the request using jQuery
-    $.ajax({
-        url: 'http://localhost:3000/api/messages',
-        headers: {
-            Authorization : 'Bearer ' + accessToken.accessToken
-        },
-        success: function(response) {
-            // Received messages!
-            console.log('Messages', response);
-        },
-        error: function(response) {
-            console.error(response);
+    newuser.save((err, savedUser) => {
+        if(err) {
+            console.log(err);
+            return res.status(500).send();
         }
-    });
-}
-
-const OktaJwtVerifier = require('@okta/jwt-verifier');
-
-const oktaJwtVerifier = new OktaJwtVerifier({
-    issuer: 'https://dev-598075.oktapreview.com/oauth2/default',
-    clientId: '0oah83rrgn2Ie6qfy0h7',
-    assertClaims: {
-        aud: 'api://default'
-    }
+        return res.status(200).send();
+    })
 });
 
-// The access token string, which should be obtained from the Authorization header on the request to your server
-const accessTokenString = 'eyJhbGciOiJSUzI1NiIsImtpZCI6Imk2UnRjSkxvbUg0e...';
-
-oktaJwtVerifier.verifyAccessToken(accessTokenString)
-    .then(jwt => {
-        // the token is valid
-        console.log(jwt.claims);
-    })
-    .catch(err => {
-        // a validation failed, inspect the error
-    });
+module.exports = router;
