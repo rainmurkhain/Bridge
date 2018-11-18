5b0feae3e0fe31324c629b0098f53c481df967fb
somepanda <somesweetrain@gmail.com>
2018-11-18 23:13:09 +0200

    add data push

--- public/scripts/newsListener.js
+++ public/scripts/newsListener.js
@@ -1,6 +1,6 @@
 var socket = io();
 socket.on('chat message', (msg) => {
-
+    alert("Keegi on veel siin lehel :O Ta ütles: " + msg);
 });
 socket.on('new news', (msg) => {
     alert("Keegi on veel siin lehel :O Ta ütles: " + msg);
